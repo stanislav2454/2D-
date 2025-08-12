cf31b178e416168d41afdc67f05e7fcfbd7697ac
stanislav2454 <StanislavViktorovich15@yandex.ru>
2025-08-12 10:48:08 +0100

    1я переделка. Рабочая версия

---
+++ Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// using UnityEngine;
+
+//[RequireComponent(typeof(CapsuleCollider), typeof(Runner), typeof(Crawler))]
+//public class CharacterMovement : MonoBehaviour
+//{
+//    private const int ZeroValue = 0;
+
+//    [SerializeField] private MovementSettings _settings;
+
+//   // private float _currentMoveSpeed;
+//    private Runner _runner;
+//    private Crawler _crawler;
+
+//    private void Awake()
+//    {
+//        _runner = GetComponent<Runner>();
+//        _crawler = GetComponent<Crawler>();
+//    }
+
+//    public void Move(float horizontalDirection, float verticalDirection)
+//    {// - не надо передавать данные между методами через поля.
+//     //   Для этого есть входные параметры и возвращаемое значение.
+//        float _currentMoveSpeed = GetCurrentSpeed();
+
+//        Vector3 direction = new Vector3(horizontalDirection, ZeroValue, verticalDirection) * _currentMoveSpeed * Time.deltaTime;
+//        transform.Translate(direction);
+//    }
+
+//    private float GetCurrentSpeed()
+//    {
+//        if (_crawler.IsCrawling)
+//            return _settings.CrawlSpeed;
+
+//        if (_runner.IsRunning)
+//            return _settings.RunSpeed;
+
+//        return _settings.WalkSpeed;
+//    }
+//}
+ 
---
+++ Assets/Scripts/Jumper.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumper : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+// using UnityEngine;
+
+//[RequireComponent(typeof(Rigidbody))]
+//public class Jumper : MonoBehaviour
+//{
+//    [SerializeField] private float _jumpPower = 5;
+
+//    private Rigidbody _rigidbody;
+
+//    private void Awake()
+//    {
+//        _rigidbody = GetComponent<Rigidbody>();
+//    }
+
+//    public void Jump()
+//    {
+//        _rigidbody.AddForce(Vector3.up * _jumpPower, ForceMode.VelocityChange);
+//    }
+//}
+ 
